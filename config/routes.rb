2bc486e330a40ae8cdb9771643341cea9acbf24e
Annie Pennell <annie@anniepennell.com>
2015-04-27 00:37:07 -0700

    added checkboxes, edit, delete, and mark complete features

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,25 @@ Rails.application.routes.draw do
   # handle submitted form
   post '/', to: 'todos#create'
 
+  # mark item completed
+  match '/todos/complete', to: 'todos#complete', :via => :post
+
+  # get selected item to delete
+  get '/todos/:id/delete', to: 'todos#destroy', as: :delete_todos
+
+  # delete selected item
+  delete '/todos/:id/delete', to: 'todos#destroy'
+
+  # get completed items to delete
+  get '/todos/delete', to: 'todos#destroy_complete', as: :delete_complete_todos
+
+  # delete completed items
+  delete '/todos/delete', to: 'todos#destroy_complete'
+
+  # show edit paeg with selected item to edit
+  get '/todos/:id/edit', to: 'todos#edit', as: :edit_todos
+
+  # update edited item
+  patch '/todos/:id', to: 'todos#update', as: :todo
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424204903) do
+ActiveRecord::Schema.define(version: 20150426035855) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "completed"
   end
 
 end
